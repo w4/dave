3ea1f660120a17cb375a39e839e17d4872b3dbd2
dave <dave@ip-172-26-11-212.eu-west-2.compute.internal>
2019-03-03 23:48:28 +0000

    donk acronyms are funny

--- dave/modules/donk.py
+++ dave/modules/donk.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """Reply with donk music whenever anyone mentions donk."""
 import dave.module
+import random
 
 
 @dave.module.help("Puts a donk on it.", name="donk")
-@dave.module.match(r"(.*)donk(.*)")
+@dave.module.match(r"(.*)(d(\S+\s)o(\S+\s)n(\S+\s)k|donk)(.*)")
 @dave.module.priority(dave.module.Priority.LOW)
 def donk(bot, args, sender, source):
-    bot.msg(source, "https://www.youtube.com/watch?v=ckMvj1piK58")
+    bot.msg(source, "https://www.youtube.com/watch?v={}".format(random.choice(["ckMvj1piK58", "5dQkhKk5Kgk", "qAugqv8e2KM", "ctWh_AEA6RY", "LxXnmkNEyh4", "ckMvj1piK58"])))
