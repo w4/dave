5ccc341a97f204f07ab8687cc76b2397d2ef2207
dave <dave@ip-172-26-11-212.eu-west-2.compute.internal>
2019-03-30 14:51:51 +0000

    nice should be reserved for special occasions

--- dave/modules/nice.py
+++ dave/modules/nice.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 """Nice."""
 import dave.module
+import random
 
 
 @dave.module.help("Nice.", name="nice")
 @dave.module.match(r"^nice\.?$")
 @dave.module.priority(dave.module.Priority.LOW)
 def nice(bot, args, sender, source):
-    bot.msg(source, "nice")
+    if random.randint(1, 100) <= 10:
+        bot.msg(source, "nice")
